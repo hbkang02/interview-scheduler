c191c0e98da4883cb44f66545b981d5dbe090081
John Kang <hbkang02@gmail.com>
2023-01-10 21:03:29 -0800

    added functional buttons

--- stories/index.js
+++ stories/index.js
@@ -11,14 +11,15 @@ storiesOf("Button", module)
   .addParameters({
     backgrounds: [{ name: "dark", value: "#222f3e", default: true }]
   })
-  .add("Base", () => <Button>Base</Button>)
+  .add("Base", () => <Button onClick>Base</Button>)
   .add("Confirm", () => <Button confirm>Confirm</Button>)
   .add("Danger", () => <Button danger>Cancel</Button>)
   .add("Clickable", () => (
-    <Button onClick={action("button-clicked")}>Clickable</Button>
+    <Button onClick={action('button-clicked')}>Clickable</Button>
   ))
   .add("Disabled", () => (
     <Button disabled onClick={action("button-clicked")}>
       Disabled
     </Button>
   ));
+
